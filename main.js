111003f62317cbd1bd4435e29cc3cde2ddefc4ac
Shauryavir <72925636+Shauryavir@users.noreply.github.com>
2022-11-10 18:01:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+objects = [];
+status = "";
+
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.size(480, 380)
+
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    objectName = document.getElementById("input1").value
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }
+    else {
+        console.log(results);
+        objects = results
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status!=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#ADD8E6")
+            percent = floor(objects[i].confidence*100)
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15)
+            noFill()
+            stroke("#A8FB30")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+            if(objects[i].label == objectName){
+                video.stop()
+                objectDetector.detect(gotResult)
+                document.getElementById("status_good").innerHTML = objectName  + " found"
+            }
+            else{
+                document.getElementById("status").innerHTML = objectName + " not found"
+            }
+        }
+    }
+ 
+}
